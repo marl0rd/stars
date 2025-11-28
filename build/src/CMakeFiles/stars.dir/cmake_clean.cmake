1cfa862d7536063c63da34ee91ce949250dda5ce
m0 <marlontrujillo1080@gmail.com>
2025-11-27 22:43:49 -0500

    HistoryReader in progress

---
+++ build/src/CMakeFiles/stars.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stars.dir/link.d"
+  "CMakeFiles/stars.dir/HistoryReader.cpp.o"
+  "CMakeFiles/stars.dir/HistoryReader.cpp.o.d"
+  "CMakeFiles/stars.dir/main.cpp.o"
+  "CMakeFiles/stars.dir/main.cpp.o.d"
+  "stars"
+  "stars.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
